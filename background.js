8f4737663561d36362cc8bec7a072bc76a990f2a
operator <operator@noctivore.com>
2015-02-08 03:48:33 -0800

    Adds locale strings and already installed check support

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.url.indexOf('accounts.google.com/') > -1) {
+  if (tab.url.indexOf('accounts.google.com/') > -1 || tab.url.indexOf('staysignedout.com/') > -1) {
     chrome.pageAction.setIcon({
       "tabId": tabId,
       "path": "images/omnibox-icon.png"
