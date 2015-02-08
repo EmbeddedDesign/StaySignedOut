bbf2443d61429d043ba1f75f68fe75f5d6a07b40
operator <operator@noctivore.com>
2015-02-07 22:50:40 -0800

    First commit

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (tab.url.indexOf('accounts.google.com/') > -1) {
+    chrome.pageAction.setIcon({
+      "tabId": tabId,
+      "path": "images/omnibox-icon.png"
+    });
+    chrome.pageAction.setTitle({
+      "tabId": tabId,
+      "title": chrome.i18n.getMessage("hoverTitle")
+    });
+    chrome.pageAction.show(tabId);
+  }
+});
